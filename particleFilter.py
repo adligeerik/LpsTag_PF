f8e9cb9a64c3d08cfe1f4b84bea6404e5abaddde
Erik <erik.von.ke@gmail.com>
2019-07-15 18:31:31 +0200

    python script started

---
+++ particleFilter.py
@@ -0,0 +1,64 @@
+''' ddist class '''
+class Ddist:
+    __init__():
+
+''' Particle Class
+x,y,z - coordinates
+map - positions of all landmarks (acnchors)
+'''
+class Particle:
+    __init__(self,x,y,z,map)
+
+    self.x = x
+    self.y = y
+    self.z = z
+    self.landmarks = []
+   for landmark in map:
+        landmark = Ddist(self.x,self.y,self.z,landmark)
+        self.landmarks.append[landmark]
+
+
+''' Move particles '''
+def moveParticles(particles):
+
+    return particles
+
+''' Calculate ddist '''
+def updateDdist(particles):
+
+    return particles
+
+''' Normalize weight '''
+def normalizeWeight(particles):
+
+    return particles
+
+
+''' Assign Weight '''
+def assignWeight(particles):
+
+    return particles
+
+''' Init
+m - number of particles
+'''
+def init(m):
+
+    return particles
+
+
+''' Low variance sampling '''
+def lowVarianceSampling(particles):
+
+    return particles
+
+
+''' Particle filter '''
+def particleFilter(particles):
+
+    return particles, mu
+
+
+''' Main '''
+def main():
+    while (data):
