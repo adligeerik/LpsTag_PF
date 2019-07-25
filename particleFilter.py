aaa22d42a10dd30791385be663ce3164fde6938d
Erik <erik.von.ke@gmail.com>
2019-07-25 23:52:43 +0200

    prticle as list instead of clas

--- particleFilter.py
+++ particleFilter.py
@@ -13,47 +13,23 @@ import copy
 '''{"id":"0x4909","ts":"160253.5610","msgid":3767147,"usb":0,"acc":"[10.27,0.891,0.364]","gyro":"[0.2,0.3,-1.2]","mag":[122,127,215],"ref_anchor":"5b3","meas":[{"addr":"5b3","ddist":"-0.051","tqf":1,"rssi":"-79.0"},{"addr":"4a30","ddist":"1.126","tqf":1,"rssi":"-78.5"},{"addr":"611","ddist":"-1.725","tqf":1,"rssi":"-78.9"}]}'''
 
 
-''' Particle Class
-x,y,z - coordinates
-map - positions of all landmarks (acnchors)
-'''
-class Particle:
-    def __init__(self,x,y,z,map,reafAnchor):
+def calculateDdist(particle, anchorMap):
+    # Distance to the ref_anchor (ref_ancor is in 0,0,0 by deffenition)
 
-        # Coordinates of particle
-        self.x = x
-        self.y = y
-        self.z = z
+    ddistDict = {}
+    refAnchorDist = math.sqrt(particle[0]**2 + particle[1]**2 + particle[2]**2)
 
-        # Weight of particle (init 0)
-        self.weight = 0
+    # Calculate the ddist to all other anchors. ddist is the difference in lenght from the ref_anchor to another anchor.
+    for anchor in anchorMap:
+        # Coordinate for that anchor (landmark)
+        pos = anchorMap[anchor]
 
-        # Coordinates of the anchors in the system (all of them)
-        self.map = map
+        # Euclidean distance differnece from with regards to the ref_anchor
+        ddist = math.sqrt((pos['x']-particle[0]**2)**2 + (pos['y']-particle[1]**2)**2 + (pos['z']-particle[2]**2)**2)
+        ddist = ddist - refAnchorDist
+        ddistDict[anchor] = ddist
 
-        # Dictionary with all ddist for the particle
-        # key = anchor id, val = ddist
-        self.ddistDict = OrderedDict() 
-
-        # The id of the master anchor
-        self.refAnchor = reafAnchor
-
-        # Calculates the ddist for the particle
-        self.calculateDdist()
-
-    def calculateDdist(self):
-        # Distance to the ref_anchor (ref_ancor is in 0,0,0 by deffenition)
-        refAnchorDist = math.sqrt(self.x**2 + self.y**2 + self.z**2)
-
-        # Calculate the ddist to all other anchors. ddist is the difference in lenght from the ref_anchor to another anchor.
-        for anchor in self.map:
-            # Coordinate for that anchor (landmark)
-            pos = self.map[anchor]
-
-            # Euclidean distance differnece from with regards to the ref_anchor
-            ddist = math.sqrt((pos['x']-self.x)**2 + (pos['y']-self.y)**2 + (pos['z']-self.z)**2)
-            ddist = ddist - refAnchorDist
-            self.ddistDict[anchor] = ddist
+    return ddistDict
 
 
 ''' Move particles 
@@ -84,9 +60,9 @@ def moveParticles(particles,acceleration,timestamp):
     varAcc = 1
 
     for particle in particles:
-        particle.x = particle.x + dx + gauss(0,varAcc)/10
-        particle.y = particle.y + dy + gauss(0,varAcc)/10
-        particle.z = particle.z + dz + gauss(0,varAcc)/10
+        particle[0] = particle[0] + dx + gauss(0,varAcc)/10
+        particle[1] = particle[1] + dy + gauss(0,varAcc)/10
+        particle[2] = particle[2] + dz + gauss(0,varAcc)/10
 
     return 0
 
@@ -94,11 +70,6 @@ def moveParticles(particles,acceleration,timestamp):
 def updateMap(particles):
     return 0
 
-''' Calculate ddist '''
-def updateDdist(particles):
-    for particle in particles:
-        particle.calculateDdist()
-    
 
 ''' Normalize weight 
 Calculates the sum of all weights and normalize the weight for all particles'''
@@ -107,16 +78,16 @@ def normalizeWeight(particles):
     weightSum = 0
     # Sumation of all weights
     for particle in particles:
-        weightSum = particle.weight + weightSum
+        weightSum = particle[3] + weightSum
 
     # Normalization of the weights
     for particle in particles:
-        particle.weight = particle.weight / weightSum
+        particle[3] = particle[3] / weightSum
 
 
 
 ''' Assign Weight '''
-def assignWeight(particles,measurement):
+def assignWeight(particles,anchorMap,measurement):
     
     anchorOrder = []
     mean = []
@@ -138,15 +109,15 @@ def assignWeight(particles,measurement):
     highP = 0
     for particle in particles:
         ddist = []
+        ddistval = calculateDdist(particle,anchorMap)
         for anchor in anchorOrder:
-            ddist.append(particle.ddistDict[anchor])
+            ddist.append(ddistval[anchor])
         
         
         mean = map(float,mean)
-
         #Calculates the probability of that particle
         p = multivariate_normal.pdf(ddist, mean, cov)
-        particle.weight = p
+        particle[3] = p
         if p > highP:
             highP = p
     print("highest p: " + str(highP))
@@ -172,7 +143,7 @@ def init(numParticles,map,minmax):
 
         # Create a particle
         try:
-            particle = Particle(x,y,z,map,refAnchor)
+            particle = [x,y,z,0]
         except UnboundLocalError:
             print(UnboundLocalError)
             print("\n Problaby no ref_anchor in map \n")
@@ -191,13 +162,13 @@ def lowVarianceSampling(particles):
     M = len(particles)
     r = uniform(0,1.0/M)
     inM = 1.0/M
-    c = particles[0].weight
+    c = particles[0][3]
     i = 0
     for m in range(M):
         U = r + m*inM
         while( U > c ):
             i = i + 1
-            c = c + particles[i].weight
+            c = c + particles[i][3]
         newParticles.append(copy.deepcopy(particles[i]))
 
     return(newParticles)
@@ -225,9 +196,9 @@ def meanPos(particles):
     mu["y"] = 0
     mu["z"] = 0
     for particle in particles:
-        mu["x"] = particle.x + mu["x"]
-        mu["y"] = particle.y + mu["y"]
-        mu["z"] = particle.z + mu["z"]
+        mu["x"] = particle[0] + mu["x"]
+        mu["y"] = particle[1] + mu["y"]
+        mu["z"] = particle[2] + mu["z"]
 
     M = len(particles)
 
@@ -237,6 +208,24 @@ def meanPos(particles):
 
     return mu
 
+''' Weighted average'''
+def weightedAverage(particles):
+
+    mu = {}
+    mu["x"] = 0
+    mu["y"] = 0
+    mu["z"] = 0
+    for particle in particles:
+        mu["x"] = particle[0]*particle[3] + mu["x"]
+        mu["y"] = particle[1]*particle[3] + mu["y"]
+        mu["z"] = particle[2]*particle[3] + mu["z"]
+
+    M = len(particles)
+
+    mu["x"] = mu["x"]/M
+    mu["y"] = mu["y"]/M
+    mu["z"] = mu["z"]/M
+    return mu
 
 ''' Most likely position
 The most likely position of the tag given all the particles'''
@@ -246,7 +235,7 @@ def bestPos(particles):
     return mu
 
 ''' Particle filter '''
-def particleFilter(particles,dataPackage):
+def particleFilter(particles,anchorMap,dataPackage):
 
     try:
         measurement = dataPackage["meas"]
@@ -264,7 +253,7 @@ def particleFilter(particles,dataPackage):
     timestep = dataPackage["ts"]
 
     # Calculate weight
-    assignWeight(particles,measurement)
+    assignWeight(particles,anchorMap,measurement)
 
     # Normalize weight
     normalizeWeight(particles)
@@ -274,9 +263,6 @@ def particleFilter(particles,dataPackage):
     
     # Move particles
     moveParticles(particles,acceleration,timestep)    
-
-    # Updates the ddist for all particles
-    updateDdist(particles)
 
     # Gives a extimated position from the particles
     mu = bestPos(particles)
@@ -363,31 +349,31 @@ def main():
             continue
         
         # Draw anchors
-        for i, anchor in enumerate(anchorMap):
-            ax.scatter(anchorMap[anchor]["x"],anchorMap[anchor]["y"],anchorMap[anchor]["z"], c='blue')
-            ax.text(anchorMap[anchor]["x"],anchorMap[anchor]["y"],anchorMap[anchor]["z"], anchor)
+        #for i, anchor in enumerate(anchorMap):
+        #    ax.scatter(anchorMap[anchor]["x"],anchorMap[anchor]["y"],anchorMap[anchor]["z"], c='blue')
+        #    ax.text(anchorMap[anchor]["x"],anchorMap[anchor]["y"],anchorMap[anchor]["z"], anchor)
         
         # Call the particle filter
-        (particles,mu) = particleFilter(particles,dataPackage)
+        (particles,mu) = particleFilter(particles,anchorMap,dataPackage)
         
         # Display the particles
         #for particle in particles:
-        #    ax.scatter(particle.x,particle.y,particle.z, c='red')
+        #    ax.scatter(particle[0],particle[1],particle[2], c='red')
 
         xpos = []
         ypos = []
         zpos = []
         for particle in particles:
-            xpos.append(particle.x)
-            ypos.append(particle.y)
-            zpos.append(particle.z)
+            xpos.append(particle[0])
+            ypos.append(particle[1])
+            zpos.append(particle[2])
         xvar = np.var(xpos)
         yvar = np.var(ypos)
         zvar = np.var(zpos)
 
         print("Variance, x: "+str(xvar)+", y: "+str(yvar)+", z: "+str(zvar))
 
-        ax.scatter(mu["x"],mu["y"],mu["z"], c='green')
+        #ax.scatter(mu["x"],mu["y"],mu["z"], c='green')
 
         coord[0].append(mu["x"])
         coord[1].append(mu["y"])
@@ -400,6 +386,13 @@ def main():
         fig.canvas.draw()
         fig.canvas.flush_events()
 
+        if index > 4000:
+            break
+
+
+    for i, anchor in enumerate(anchorMap):
+        ax.scatter(anchorMap[anchor]["x"],anchorMap[anchor]["y"],anchorMap[anchor]["z"], c='blue')
+        ax.text(anchorMap[anchor]["x"],anchorMap[anchor]["y"],anchorMap[anchor]["z"], anchor)
 
     ax.plot(coord[0],coord[1],coord[2], c='green')
     fig.canvas.draw()
